6d9330bd16acb8e44525b572b47464b21f21f83b
Hassan Syed <hassan.syed@ukplc.net>
2011-08-08 17:08:57 +0100

    split out the protocol implementation service into an object class

--- include/rpc/server/backend/BackEndBase.h
+++ include/rpc/server/backend/BackEndBase.h
@@ -71,152 +71,35 @@ namespace rubble { namespace rpc {
   public:
     typedef ClientCookieBase t_client_cookie;
 
-    void init(boost::system::error_code & ec) {ec.clear();}
-    void teardown(boost::system::error_code & ec) {ec.clear();}
+    void init ( boost::system::error_code & ec);
+    void teardown ( boost::system::error_code & ec);
  
-    void hello(ClientCookie & cc,ClientData & cd,
-      basic_protocol::HelloRequest & hr, basic_protocol::HelloResponse & hres)
-    { 
-      if( m_backend->destination_type() != hr.expected_target() )
-      {
-        cd.error_code().assign(
-          error_codes::RBL_BACKEND_CLIENT_TARGET_TYPE_MISMATCH,
-          rpc_backend_error);
-        cd.request_disconect();
-        hres.set_error_type(basic_protocol::DESTINATION_EXPECTATION_MISMATCH);
-        return;
-      }
+    void hello (
+        ClientCookie & cc, ClientData & cd,
+        basic_protocol::HelloRequest & hr, basic_protocol::HelloResponse & hres);
     
-      if( m_backend->source_type() != hr.source_type())
-      {
-        cd.error_code().assign(
-          error_codes::RBL_BACKEND_CLIENT_SOURCE_TYPE_MISMATCH,
-          rpc_backend_error);
-        hres.set_error_type(basic_protocol::SOURCE_EXPECTATION_MISMATCH);
-        cd.request_disconect();
-        return;
-      }
+    void list_services (  
+        ClientCookie & cc ,ClientData & cd,
+        basic_protocol::ListServicesRequest & req, 
+        basic_protocol::ListServicesResponse & res);
 
-      cd.name(hr.node_name());
-      hres.set_error_type(basic_protocol::NO_HELLO_ERRORS);
-      cd.establish_client();
-    }
+    void subscribe (
+        ClientCookie & client_cookie, ClientData & cd,
+        std::string *,std::string *); 
+    
+    void unsubscribe ( ClientCookie & client_cookie, ClientData & cd);
 
-    void list_services(  ClientCookie & cc ,ClientData & cd,
-                        basic_protocol::ListServicesRequest & req, 
-                        basic_protocol::ListServicesResponse & res)
-    {
-      const BackEndBase::t_services & services = m_backend->services();
+    void rpc_subscribe_service ( 
+        ClientCookie & cc,ClientData & cd,
+        basic_protocol::SubscribeServiceRequest & req,
+        basic_protocol::SubscribeServiceResponse & res);
 
-      for(int i = 0; i < services.size(); ++i)
-      {
-        basic_protocol::ServiceEntry * s_e = res.add_services(); 
-        const BackEndBase::t_services::entry_type * b_s_e = services.EntryAtordinal(i);
-        s_e->set_service_ordinal( b_s_e->ordinal());
-        s_e->set_service_name( b_s_e->name().c_str());
-      } 
-    }
-    void subscribe(ClientCookie & client_cookie, ClientData & cd,
-      std::string *,std::string *) {}
-    void unsubscribe(ClientCookie & client_cookie, ClientData & cd) {}
-
-    void rpc_subscribe_service( ClientCookie & cc,ClientData & cd,
-                                basic_protocol::SubscribeServiceRequest & req,
-                                basic_protocol::SubscribeServiceResponse & res)
-    { 
-      BackEndBase::t_services & services = m_backend->services();
-      
-      if( !( req.service_ordinal() < services.size()))
-      {
-        cd.error_code().assign( 
-          error_codes::RBL_BACKEND_SUBSCRIBE_NO_SERVICE_WITH_ORDINAL,
-          rpc_backend_error);
-        res.set_error(basic_protocol::SERVICE_ORDINAL_NOT_IN_USE);
-        return;
-      }
-
-      ClientServiceCookies & cookies = m_backend->cookies(); 
-      ClientCookie * cookie;
-      { 
-        boost::recursive_mutex & mutex = m_backend->mutex();
-        boost::unique_lock<boost::recursive_mutex> lock(mutex);
-        cookies.create_or_retrieve_cookie(
-          req.service_ordinal(), &cd, &cookie);
-      }
-      
-      //subcription should only occur once
-      if(cookie->is_subscribed())
-      {
-        cd.error_code().assign(
-          error_codes::RBL_BACKEND_ALLREADY_SUBSCRIBED,
-          rpc_backend_error);
-        res.set_error(basic_protocol::SERVICE_ALLREADY_SUBSCRIBED);
-        return;
-      } 
-
-      ServiceBase::shp * s = services[req.service_ordinal()];
-
-      BOOST_ASSERT_MSG(s != NULL, 
-      "THERE SHOULD BE A SERVICE IN THE CONTEXT OF A " 
-      "CLIENT-SERVICE-SUBSCRIPTION");
-      
-      std::string * req_s = NULL;  
-      if(req.has_subscribe_request_string())
-        req_s = req.mutable_subscribe_request_string();
- 
-      std::string * res_s = 
-        res.mutable_subscribe_result_string();
-      
-      (*s)->subscribe (*cookie,cd,req_s,res_s);
-
-      if(!cd.error_code())
-      {
-        cookie->subscribe();
-        res.set_error(basic_protocol::NO_SUBSCRIBE_SERVICE_ERROR);
-      }
-    }
-
-    void rpc_unsubscribe_service( ClientCookie & cc,ClientData & cd,
-                                basic_protocol::UnsubscribeServiceRequest & req,
-                                basic_protocol::UnsubscribeServiceResponse & res)
-    {
-      boost::recursive_mutex & mutex = m_backend->mutex();
-      boost::unique_lock<boost::recursive_mutex> lock(mutex);
-
-      BackEndBase::t_services & services = m_backend->services();
-
-      if( !( req.service_ordinal() < services.size()))
-      {
-        cd.error_code().assign( 
-          error_codes::RBL_BACKEND_SUBSCRIBE_NO_SERVICE_WITH_ORDINAL,
-          rpc_backend_error);
-        res.set_error(basic_protocol::SERVICE_ORDINAL_NOT_IN_USE);
-        return;
-      }
-      
-      
-      ClientServiceCookies & cookies = m_backend->cookies(); 
-      ClientCookie * cookie;
-      cookies.create_or_retrieve_cookie( req.service_ordinal(), &cd, &cookie);
-      
-      if(! cookie->is_subscribed())
-      {
-        cd.error_code().assign(
-          error_codes::RBL_BACKEND_NOT_SUBSCRIBED,
-          rpc_backend_error);
-        res.set_error(basic_protocol::NO_SUBSCRIPTION_FOR_SERVICE);
-        return;
-      }
-
-      ServiceBase::shp & s = * services[req.service_ordinal()];
-      s->unsubscribe(*cookie,cd);
-
-      cookie->destroy_cookie();
-      cookies.delete_cookie(req.service_ordinal(),&cd);
-    }
-
-    void backend(BackEndBase * backend)
-      { m_backend = backend; }
+    void rpc_unsubscribe_service ( 
+        ClientCookie & cc,ClientData & cd,
+        basic_protocol::UnsubscribeServiceRequest & req,
+        basic_protocol::UnsubscribeServiceResponse & res);
+    
+     void backend ( BackEndBase * backend );
   private:
      BackEndBase * m_backend;
   };
--- src/rpc/server/BackEndBase.cc
+++ src/rpc/server/BackEndBase.cc
@@ -1,6 +1,9 @@
 #include <rpc/server/backend/BackEndBase.h>
 
 namespace rubble { namespace rpc {
+// BackEndBase ////////////////////////////////////////////////////////////////
+
+  // BackEndBase //////////////////////////////////////////////////////////////
   BackEndBase::BackEndBase( 
     basic_protocol::SourceConnectionType       source_type,
     basic_protocol::DestinationConnectionType backend_type) 
@@ -20,7 +23,9 @@ namespace rubble { namespace rpc {
       
       register_and_init_service(sb_shp);
     }
+  //-------------------------------------------------------------------------//
 
+  // start ////////////////////////////////////////////////////////////////////
   void BackEndBase::start()
   {
     BOOST_ASSERT_MSG((m_pool_size > 0), "thread pool must have >=1 threads");
@@ -48,7 +53,9 @@ namespace rubble { namespace rpc {
         boost::bind( &boost::asio::io_service::run, &m_io_service) );
     }
   }
-  
+  //-------------------------------------------------------------------------//
+
+  //  register_and_init_service ///////////////////////////////////////////////
   void BackEndBase::register_and_init_service(ServiceBase::shp service)
   {
     if(!m_is_sealed)
@@ -77,7 +84,9 @@ namespace rubble { namespace rpc {
         <<  rbl_backend_error_code( boost::system::error_code( 
               error_codes::RBL_RPC_BACKEND_SEALED, rpc_backend_error));
   }
+  //-------------------------------------------------------------------------//
   
+  // block_till_termination ///////////////////////////////////////////////////
   void BackEndBase::block_till_termination()
   {
     m_thread_group.join_all();
@@ -117,13 +126,17 @@ namespace rubble { namespace rpc {
 
     return m_io_service.stopped();
   }
-
+  //-------------------------------------------------------------------------//
+  
+  // connect //////////////////////////////////////////////////////////////////
   void BackEndBase::connect(ClientData::shp & client_data)
   {
     boost::lock_guard<boost::recursive_mutex> lock(m_mutex);
     m_connected_clients.insert(client_data);
   }
-  
+  //-------------------------------------------------------------------------//
+
+  // disconect ////////////////////////////////////////////////////////////////
   void BackEndBase::disconect(ClientData::shp & client_data)
   {
     boost::lock_guard<boost::recursive_mutex> lock(m_mutex);
@@ -150,5 +163,197 @@ namespace rubble { namespace rpc {
  
     m_connected_clients.erase(client_data);
   }
+  //-------------------------------------------------------------------------//
+//---------------------------------------------------------------------------//
 
+// BasicProtocolImpl //////////////////////////////////////////////////////////
+  // init /////////////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::init(boost::system::error_code & ec) 
+  {
+    ec.clear();
+  }
+  //-------------------------------------------------------------------------//
+
+  // teardown /////////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::teardown(boost::system::error_code & ec) 
+  {
+    ec.clear();
+  }
+  //-------------------------------------------------------------------------//
+
+  // hello ////////////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::hello(ClientCookie & cc,ClientData & cd,
+      basic_protocol::HelloRequest & hr, basic_protocol::HelloResponse & hres)
+  { 
+    if( m_backend->destination_type() != hr.expected_target() )
+    {
+      cd.error_code().assign(
+        error_codes::RBL_BACKEND_CLIENT_TARGET_TYPE_MISMATCH,
+        rpc_backend_error);
+      cd.request_disconect();
+      hres.set_error_type(basic_protocol::DESTINATION_EXPECTATION_MISMATCH);
+      return;
+    }
+  
+    if( m_backend->source_type() != hr.source_type())
+    {
+      cd.error_code().assign(
+        error_codes::RBL_BACKEND_CLIENT_SOURCE_TYPE_MISMATCH,
+        rpc_backend_error);
+      hres.set_error_type(basic_protocol::SOURCE_EXPECTATION_MISMATCH);
+      cd.request_disconect();
+      return;
+    }
+
+    cd.name(hr.node_name());
+    hres.set_error_type(basic_protocol::NO_HELLO_ERRORS);
+    cd.establish_client();
+  }
+  //-------------------------------------------------------------------------//
+  
+  // list_services ////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::list_services(  
+    ClientCookie & cc, ClientData & cd,
+    basic_protocol::ListServicesRequest & req, 
+    basic_protocol::ListServicesResponse & res)
+  {
+    const BackEndBase::t_services & services = m_backend->services();
+
+    for(int i = 0; i < services.size(); ++i)
+    {
+      basic_protocol::ServiceEntry * s_e = res.add_services(); 
+      
+      const BackEndBase::t_services::entry_type * b_s_e = 
+        services.EntryAtordinal(i);
+      
+      s_e->set_service_ordinal( b_s_e->ordinal());
+      s_e->set_service_name( b_s_e->name().c_str());
+    } 
+  }
+  //-------------------------------------------------------------------------//
+
+  // subscribe ////////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::subscribe(  ClientCookie & client_cookie, 
+                                      ClientData & cd,std::string *,
+                                      std::string *) 
+  {
+
+  }
+  //-------------------------------------------------------------------------//
+
+  // unsubscribe //////////////////////////////////////////////////////////////
+  void BasicProtocolImpl::unsubscribe(  ClientCookie & client_cookie, 
+                                        ClientData & cd) 
+  {
+  }
+  //-------------------------------------------------------------------------//
+
+  // rpc_subscribe_service ////////////////////////////////////////////////////
+  void BasicProtocolImpl::rpc_subscribe_service( 
+    ClientCookie & cc, ClientData & cd,
+    basic_protocol::SubscribeServiceRequest & req,
+    basic_protocol::SubscribeServiceResponse & res)
+  { 
+    BackEndBase::t_services & services = m_backend->services();
+    
+    if( !( req.service_ordinal() < services.size()))
+    {
+      cd.error_code().assign( 
+        error_codes::RBL_BACKEND_SUBSCRIBE_NO_SERVICE_WITH_ORDINAL,
+        rpc_backend_error);
+      res.set_error(basic_protocol::SERVICE_ORDINAL_NOT_IN_USE);
+      return;
+    }
+    
+    ClientServiceCookies & cookies = m_backend->cookies(); 
+    ClientCookie * cookie;
+    { 
+      boost::recursive_mutex & mutex = m_backend->mutex();
+      boost::unique_lock<boost::recursive_mutex> lock(mutex);
+      cookies.create_or_retrieve_cookie(
+        req.service_ordinal(), &cd, &cookie);
+    }
+    
+    //subcription should only occur once
+    if(cookie->is_subscribed())
+    {
+      cd.error_code().assign(
+        error_codes::RBL_BACKEND_ALLREADY_SUBSCRIBED,
+        rpc_backend_error);
+      res.set_error(basic_protocol::SERVICE_ALLREADY_SUBSCRIBED);
+      return;
+    } 
+
+    ServiceBase::shp * s = services[req.service_ordinal()];
+
+    BOOST_ASSERT_MSG(s != NULL, 
+    "THERE SHOULD BE A SERVICE IN THE CONTEXT OF A " 
+    "CLIENT-SERVICE-SUBSCRIPTION");
+    
+    std::string * req_s = NULL;  
+    if(req.has_subscribe_request_string())
+      req_s = req.mutable_subscribe_request_string();
+
+    std::string * res_s = 
+      res.mutable_subscribe_result_string();
+    
+    (*s)->subscribe (*cookie,cd,req_s,res_s);
+
+    if(!cd.error_code())
+    {
+      cookie->subscribe();
+      res.set_error(basic_protocol::NO_SUBSCRIBE_SERVICE_ERROR);
+    }
+  }
+  //-------------------------------------------------------------------------//
+
+  // rpc_unsubscribe_service //////////////////////////////////////////////////
+  void BasicProtocolImpl::rpc_unsubscribe_service ( 
+    ClientCookie & cc,ClientData & cd,
+    basic_protocol::UnsubscribeServiceRequest & req,
+    basic_protocol::UnsubscribeServiceResponse & res )
+  {
+    boost::recursive_mutex & mutex = m_backend->mutex();
+    boost::unique_lock<boost::recursive_mutex> lock(mutex);
+
+    BackEndBase::t_services & services = m_backend->services();
+
+    if( !( req.service_ordinal() < services.size()))
+    {
+      cd.error_code().assign( 
+        error_codes::RBL_BACKEND_SUBSCRIBE_NO_SERVICE_WITH_ORDINAL,
+        rpc_backend_error);
+      res.set_error(basic_protocol::SERVICE_ORDINAL_NOT_IN_USE);
+      return;
+    }
+    
+    
+    ClientServiceCookies & cookies = m_backend->cookies(); 
+    ClientCookie * cookie;
+    cookies.create_or_retrieve_cookie( req.service_ordinal(), &cd, &cookie);
+    
+    if(! cookie->is_subscribed())
+    {
+      cd.error_code().assign(
+        error_codes::RBL_BACKEND_NOT_SUBSCRIBED,
+        rpc_backend_error);
+      res.set_error(basic_protocol::NO_SUBSCRIPTION_FOR_SERVICE);
+      return;
+    }
+
+    ServiceBase::shp & s = * services[req.service_ordinal()];
+    s->unsubscribe(*cookie,cd);
+
+    cookie->destroy_cookie();
+    cookies.delete_cookie(req.service_ordinal(),&cd);
+  }
+  //-------------------------------------------------------------------------//
+  
+  // backend ////////////////////////////////////////////////////////////////// 
+  void BasicProtocolImpl::backend(BackEndBase * backend)
+  { 
+    m_backend = backend; 
+  }
+  //-------------------------------------------------------------------------//
+//---------------------------------------------------------------------------//
 } }
